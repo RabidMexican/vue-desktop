7da79864dc6cbf1f39e32db464a0c6faec0f7231
RabidMexican <rabidmexican@gmail.com>
2021-02-10 12:31:14 +0100

    added system clock

---
+++ src/components/Clock.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="cutout px-1">
+        {{ hour + ':' + minute + ':' + second }}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Clock',
+        data() {
+            return {
+                hour: 0,
+                minute: 0,
+                second: 0,
+            }
+        },
+        mounted() {
+            this.getTime();
+            this.timer = setInterval(this.getTime, 1000);
+        },
+        methods: {
+            getTime() {
+                let now = new Date();
+                this.hour = this.format(now.getHours());
+                this.minute = this.format(now.getMinutes());
+                this.second = this.format(now.getSeconds());
+            },
+            format(number) {
+                number = number.toString();
+                if(number.length < 2) return '0' + number;
+                return number;
+            }
+        }
+    }
+</script>
--- src/components/Taskbar.vue
+++ src/components/Taskbar.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="window w-full fixed bottom-0 h-10 flex items-center">
+    <div class="window w-full fixed bottom-0 h-10 px-1 flex items-center justify-between">
         <WinButton text="Start" icon="/assets/windows-logo.png"/>
+        <Clock/>
     </div>
 </template>
 
 <script>
     import WinButton from './WinButton';
+    import Clock from './Clock';
 
     export default {
         name: 'Taskbar',
         components: {
-            WinButton
+            WinButton,
+            Clock,
         }
     }
 </script>
